c310d3dd4f3d992e7577fffc25fe3060d56f0aed
GiovannaBona <125615963+GiovannaBona@users.noreply.github.com>
2023-09-01 22:13:09 -0300

    Add files via upload

---
+++ atv2.js
@@ -0,0 +1,82 @@
+// Exercício 1
+var favoriteFood = 'Pizza';
+
+// Exercício 2
+var numOfSlices = 8;
+
+// Exercício 3
+var favoriteFood = 'Pizza';
+console.log("Minha comida favorita: " + favoriteFood);
+
+// Exercício 4
+var numOfSlices = 8;
+console.log("Quantos pedaços ela tem: " +  numOfSlices);
+
+// Exercício 5
+console.log("var-> As variáveis declaradas com var têm escopo de função ou escopo global. Isso significa que elas são acessíveis em todo o escopo da função em que foram declaradas, independentemente de estarem dentro de blocos (como loops ou condicionais).")
+console.log("let-> As variáveis declaradas com let têm escopo de bloco. Isso significa que elas são acessíveis apenas dentro do bloco em que foram declaradas. Isso ajuda a evitar vazamentos de variáveis e torna o código mais previsível.")
+
+
+// Exercício 6, 7 e 8
+let changeME = true;
+ changeME = false;
+ console.log(changeME);
+
+
+// Exercício 9 e 10
+const entree = "Enchiladas";
+console.log(entree);
+
+
+// Exercício 11
+ entree = 'BOM';
+ console.log(entree);
+
+// Exercício 12
+let levelup = 0; 
+levelup += 5;  
+console.log(levelup);  
+
+
+// Exercício 13
+let powerlevel = 500;
+powerlevel -= 100;  
+console.log(powerlevel);  
+
+
+// Exercício 14
+let multiplyeu = 7; 
+multiplyeu *= 11;  
+console.log(multiplyeu); 
+
+
+// Exercício 15
+let quarterme = 20; 
+quarterme /= 4; 
+console.log(quarterme); 
+
+
+// Exercício 16
+let gaineddolar = 1; 
+gaineddolar++;    
+console.log(gaineddolar);  
+
+
+// Exercício 17
+let Dolarperdido = 10; 
+Dolarperdido--;   
+console.log(Dolarperdido);  
+
+
+// Exercício 18
+const myName = "SeuNome"; 
+const myCity = "SuaCidade"; 
+const frase = `My name is ${myName}. My favorite city is ${myCity}.`;
+console.log(frase); 
+
+
+// Exercício 19
+let newVariable = "Hello, World!"; 
+console.log(typeof newVariable); 
+
+
